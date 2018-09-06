1d4cfacbc13299fc7b5379c14cba0caf71b5a570
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-09-06 09:19:19 -0400

    Extra_End completed

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -11,9 +11,12 @@ def make_tags(tag, word):
   return "<"+tag+">" + word + "</"+tag+">"
 def make_out_word(out, word):
   return out[0:2]+word+out[-2:]
+def extra_end(str):
+  return str[-2:]+str[-2:]+str[-2:]
 
 print(hello_name("Bob"))
 print(make_abba("Hello","World"))
 print(make_tags("i", "Yay"))
 print(make_out_word("<<>>", "Yay"))
+print(extra_end("Hello"))
 

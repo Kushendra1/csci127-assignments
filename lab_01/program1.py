33a9c0e0903fbe0f5439fdea29e0d1a983f52ae7
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-09-06 09:00:52 -0400

    Make tags completed

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -7,7 +7,10 @@ def hello_name(name):
   return "Hello "+name+"!"
 def make_abba(a, b):
   return a+b+b+a
+def make_tags(tag, word):
+  return "<"+tag+">" + word + "</"+tag+">"
 
 print(hello_name("Bob"))
 print(make_abba("Hello","World"))
+print(make_tags("i", "Yay"))
 

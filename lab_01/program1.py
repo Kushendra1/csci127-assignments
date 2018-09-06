42082d5671ab27e5c04408a695e357488c184b80
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-09-06 08:51:33 -0400

    Hello World and abba finished

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -1,6 +1,13 @@
 def greeter(name):
     return "Hello " +name+"!"
-print(greeter("Stan"))
-print(greeter("Ollie"))
-print(greeter("Tom"))
+
+
+
+def hello_name(name):
+  return "Hello "+name+"!"
+def make_abba(a, b):
+  return a+b+b+a
+
+print(hello_name("Bob"))
+print(make_abba("Hello","World"))
 

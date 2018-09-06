3fbd48e54b1763360c842e5a33036ca9308eceb1
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-09-06 09:14:48 -0400

    Make out word finished

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -9,8 +9,11 @@ def make_abba(a, b):
   return a+b+b+a
 def make_tags(tag, word):
   return "<"+tag+">" + word + "</"+tag+">"
+def make_out_word(out, word):
+  return out[0:2]+word+out[-2:]
 
 print(hello_name("Bob"))
 print(make_abba("Hello","World"))
 print(make_tags("i", "Yay"))
+print(make_out_word("<<>>", "Yay"))
 

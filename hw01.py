f631a2e4f79d90e59858550739225db686ceea15
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-09-13 18:48:49 -0400

    Problem 2 done

--- hw01.py
+++ hw01.py
@@ -13,3 +13,18 @@ def capitalize_name(name):
     return cap_first+name[1:space_index]+cap_sec+name[(space_index+2):]
 
 print(capitalize_name("kushendra ramrup"))
+
+def init(name):
+    """
+    Input: name --> a string in the form "first last"
+    Returns: a string in the form "F. Last" where it's a capitalized first inital 
+    and capitalized last name
+    """
+    space_index = name.find(" ")
+    first_char = name[0]
+    cap_first = first_char.upper()
+    sec_char = name[space_index:(space_index+2)]
+    cap_sec = sec_char.upper()
+    return cap_first+"."+cap_sec+name[(space_index+2):]
+
+print(init("kushendra ramrup"))

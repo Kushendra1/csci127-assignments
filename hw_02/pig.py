d313b1ffe62ea24d0a43e514df91bdbedca93cb3
Kushendra Ramrup <40897858+Kushendra1@users.noreply.github.com>
2018-09-20 08:16:47 -0400

    Update pig.py

--- hw_02/pig.py
+++ hw_02/pig.py
@@ -1,5 +1,5 @@
 #Kushendra Ramrup and Madison Chen worked on this
-def part_pig_latin(name):
+def piglatinify(name):
     vowels = ("a","e","i","o","u", "A", "E", "I", "O", "I")
     first_letter = name[0]
     if first_letter in vowels:
@@ -7,5 +7,5 @@ def part_pig_latin(name):
     else:
         return name[1:] + name[0] + "ay"    
     
-print(part_pig_latin("Madison"))
-print(part_pig_latin("Allison"))
+print(piglatinify("Madison"))
+print(piglatinify("Allison"))

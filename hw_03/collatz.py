e7df68218a9213040d5071d6f3c19a28dc7e7312
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-09-27 08:43:26 -0400

    completed

---
+++ hw_03/collatz.py
@@ -0,0 +1,12 @@
+def collatz(n):
+    count = 0
+    while n != 1:
+        if n%2 == 0:
+            n = n/2
+        else:
+            n = (3*n) + 1
+        print(n)
+        count +=1
+    return "The number of steps is "+ str(count)
+
+print(collatz(7))

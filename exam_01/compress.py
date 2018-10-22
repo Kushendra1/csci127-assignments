b9e8b914eca565ef4ffc446ccd9727592a5457fc
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-10-22 10:30:46 -0400

    Itried

--- exam_01/compress.py
+++ exam_01/compress.py
@@ -1,12 +1,21 @@
 
+##def compress_word(w):
+##    word = w[1:]
+##    newstr = ""
+##    vowels = ('a', 'e', 'i', 'o', 'u')
+##    for i in w[1:]:
+##        if i in vowels:
+##            newstr = w[1:].replace(i, "")
+##    return w[0] + newstr
+
 def compress_word(w):
-    word = w[1:]
-    newstr = ""
-    vowels = ('a', 'e', 'i', 'o', 'u')
+    newword = []
+    vowels = ["A","E","I","O","U","a","e","i","o","u"]
+    newword.append(w[0])
     for i in w[1:]:
-        if i in vowels:
-            newstr = w[1:].replace(i, "")
-    return w[0] + newstr
+        if not i in vowels:
+            newword.append(i)
+    return "".join(newword)
 
 print(compress_word("apple"))
 print(compress_word("orange"))

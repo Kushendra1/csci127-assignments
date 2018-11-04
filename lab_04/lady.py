863a265472d9dce06bcb53c044634c7cd32826ac
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-11-04 17:53:01 -0500

    update

--- lab_04/lady.py
+++ lab_04/lady.py
@@ -9,4 +9,27 @@ def count(b):
             else:
                 amount[color.index(1)]+=1
     return amount
-                
+
+def happy(l, str):
+    if l < 3:
+        if n == 1 and str[0] == "_":
+            return True
+        if str[0] == str[1]:
+            return True
+        else:
+            return False
+    elif l >= 3:
+        for i in count(str):
+            if i < 2:
+                return False
+        return True
+    return True
+
+def HappyLadyBug(n,b):
+    if happy(n,b) == False:
+        return "No"
+    elif happy(n,b) == True:
+        return "Yes"
+    
+print(HappyLadyBug(3, "RB_"))
+print(HappyLadyBug(6, "RBR__B"))

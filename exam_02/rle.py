568eb921b224df4f9d2e6a75dea6e6239f40cbd9
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-11-19 08:50:18 -0500

    update

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -1,13 +1,10 @@
 def encode(s):
     d = {}
-    d["letter"] = []
-    d["amount"] = []
     for i in s:
-        if i not in d["letter"]:
-            d["letter"].append(i)
-            d["amount"].append(1)
+        if i not in d:
+            d.setdefault(i, 1)
         else:
-            d["amount"][d["letter"].index(i)]+=1
+            d[i] += 1
     return d
 
 print(encode("abbaaacddaaa"))

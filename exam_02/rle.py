1b2eee04f50dcc0b01b56a3d3771350538925b4d
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-11-19 11:39:20 -0500

    update

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -1,25 +1,36 @@
-def encode(s):
-    d = {}
-    for i in s:
-        if i not in d:
-            d.setdefault(i, 1)
-        else:
-            d[i] += 1
-    return d
+##def encode(s):
+##    d = {}
+##    for i in s:
+##        if i not in d:
+##            d.setdefault(i, 1)
+##        else:
+##            d[i] += 1
+##    return d
+##
+##print(encode("abbaaacddaaa"))
+##print(encode("abcd"))
 
+def encode(s):
+    l = []
+    count = 1
+    for i in range(0, len(s) - 1):
+        if s[i] != s[i + 1]:
+            l.append([s[i], count])
+            count = 1
+        else:
+            count += 1
+        new = [s[i], count]
+    l.append(new)
+    return l
+    
 print(encode("abbaaacddaaa"))
 print(encode("abcd"))
 
 def decode(l):
-    n = []
-    lexicon = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    k = ""
     for i in l:
-        if l[1] > len(l):
-            n.append(l[0])
-            print(l[1])
-            print(n)
-        
-list = ["A", 1]
-list2 = ["b", 2]
-print(decode(list))
-print(decode(list2))
+        k += i[0] * i[1]
+    return k
+
+print(decode([['a', 1], ['b', 2], ['a', 3], ['c', 1]]))
+print(decode([['a', 1], ['b', 1], ['c', 1], ['c', 1]]))

9a4d79740ad763923c2839869cb31ea4641db202
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-11-26 08:50:44 -0500

    solution added

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -37,18 +37,44 @@ print(decode([['a', 1], ['b', 1], ['c', 1], ['c', 1]]))
 
 #----------SOLUTION-----------#
 
-def rle1(line):
+##def rle1(line):
+##    encoded = []
+##    i = 0
+##    while i < len(line)-1:
+##        next_letter = i+1
+##        while next_letter < len(line) and line[next_letter] == line[i]:
+##            next_letter = next_letter = 1
+##        encoded.append(([next_letter-i], line[i]))
+##        i = next_letter
+##    if i == len(line)-1:
+##        encoded.append( [1, line[i]])
+##    return encoded
+##
+##print(rle1("abbaaacddaaa"))
+##print(rle1("abcd"))
+
+def rle2(line):
     encoded = []
-    i = 0
-    while i < len(line)-1:
-        next_letter = i+1
-        while next_letter < len(line) and line[next_letter] == line[i]:
-            next_letter = next_letter = 1
-        encoded.append(([next_letter-i], line[i]))
-        i = next_letter
-    if i == len(line)-1:
-        encoded.append( [1, line[i]])
+    count = 1
+    prevchar = line[0]
+    for c in line[1:]:
+        if c==prevchar:
+            count = count+1
+        else:
+            encoded.append([count,prevchar])
+            count = 1
+            prevchar = c
+    encoded.append([count,prevchar])
     return encoded
 
-print(rle1("abbaaacddaaa"))
-print(rle1("abcd"))
+print(rle2("abbaaacddaaa"))
+print(rle2("abcd"))
+
+def dec(encoded):
+    result = ""
+    for item in encoded:
+        result = result + item[0] * item[1]
+    return result
+
+print(dec([['a', 1], ['b', 2], ['a', 3], ['c', 1]]))
+print(dec([['a', 1], ['b', 1], ['c', 1], ['c', 1]]))

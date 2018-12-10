90808d3bc6c549d59a9f2da7958cf92e974732ff
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-12-10 09:06:08 -0500

    upate

--- final_1/sum.cpp
+++ final_1/sum.cpp
@@ -14,7 +14,7 @@ int sumofsquares(int a, int b) {
 
 int main(){
   std::cout << sumofsquares(5,10) << "\n";
-  std::cout << sumofsquares(4,8) << "\n";
+  std::cout << sumofsquares(5,8) << "\n";
   std::cout << " ^^ This is the sumofsquares of the Giants record :(" << "\n";
   std::cout << sumofsquares(9,13) << "\n";
 

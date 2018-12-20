650d770a6bbef5887623fca338b23a82417e0974
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-12-20 10:24:17 -0500

    gotta fix addline

--- final_2/dict.py
+++ final_2/dict.py
@@ -16,6 +16,8 @@ def addline(d,line):
     letter = new_line[0]
     d1 = {}
 ##    print(new_line.split())
+    for i in new_line.split():
+        
     for i in new_line:
         if letter in d.keys():
             d[letter].append(new_line)
@@ -28,3 +30,20 @@ print(addline(dict, "name face person"))
 dict2 = {"water" : "wet", "earth": "hard", "fire": "hot", "air" : "cool"}
 print(addline(dict2, "wind earth fire agua"))
 
+d = addline({}, "string")
+d = addline(d, "movie")
+d = addline(d, "cars")
+d = addline(d, "time")
+d = addline(d, "branch")
+
+def spellcheck(d, word):
+    new_word = word.lower()
+    if new_word[0] in d:
+        if new_word in d[new_word[0]]:
+            return True
+        else:
+            return False
+    return False
+
+    
+    

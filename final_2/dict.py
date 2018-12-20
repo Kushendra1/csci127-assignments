93a3a676a189f26a842b69ef6ed51ff33d7c0fde
Kushendra <Kushendra.Ramrup59@myhunter.cuny.edu>
2018-12-20 09:54:29 -0500

    dict

---
+++ final_2/dict.py
@@ -0,0 +1,25 @@
+#Kushendra Ramrup
+
+##def addline(d,line):
+##    d1 = {}
+##    list = []
+##    new_line = line.lower()
+##    for word in line:
+##        if word[0] in d:
+##            l.append(word)
+##        else:
+##            d1[word[0]] = word
+##    return d1
+
+def addline(d,line):
+    new_line = line.lower()
+    letter = line[0]
+    d1 = {}
+    if letter in d.keys():
+        d[letter].append(new_line)
+    else:
+        d1[letter] = [new_line]
+    return d1
+
+dict = {"name": "jack", "person": "two"}
+print(addline(dict, "name face person"))
